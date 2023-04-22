dcef1a653f6cd6e126f73426a3046b8aa701f032
prog_one <ikapustin16@gmail.com>
2023-04-22 23:30:27 +0300

    Написана вторая чать бота. В ней добавленно подключение к вк и разговорная речь бота

--- main (4).py
+++ main (4).py
@@ -79,3 +79,121 @@ def gpt_callback(text):
                                           temperature=0.5,
                                           max_tokens=1000)
     return completion.choices[0]['text']
+
+
+# Функция создания базы данных
+def create_database():
+    cursor.execute("CREATE TABLE messages(userid, message, answer, time)")
+
+
+# Функция инициализации базы данных
+
+def init_database():
+    global database, cursor
+    database = sqlite3.connect("gpt_database.db")
+    cursor = database.cursor()
+
+
+# Функция инициализации подключения к вк
+
+def init_VK():
+    global vk_session, longpoll
+    vk_session = vk_api.VkApi(token=VK_token)
+    longpoll = VkBotLongPoll(vk_session, group_id=219866377)
+
+
+# Функция инициализации бота
+
+def init_bot():
+    global translate, message_to_translate
+    for event in longpoll.listen():
+        # Прослушивание события присоединения нового пользователя к группе
+
+        if event.type == VkBotEventType.GROUP_JOIN:
+            joined = event.obj.message
+            user_id = joined['from_id']
+
+            vk_session.method('messages.send', {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                'Добро пожаловать в группу! Я бот GPT Вконтакте. Можете задать мне любой вопрос и '
+                'я попытаюсь дать на него корректный ответ. Чем я могу вам помочь?'})
+
+        # Прослушивание события получения нового сообщения
+
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            # Получение данных из сообщения
+
+            message = event.obj.message
+            user_id = message['from_id']
+            text = message['text']
+
+            vk_session.method('messages.send', {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                'Секунду, ваш запрос обрабатывается.'})
+
+            # Перевод сообщения, если пользователь утвердительно ответил на вопрос о переводе
+
+            if text.lower() == "да" and translate:
+                vk_session.method('messages.send', {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                    f'Переведи на русский язык сообщение: {gpt_callback(message_to_translate)}'})
+                translate = False
+            else:
+                if detect_language(text) == "en":
+                    message_to_translate = text
+                    translate = True
+                # Обработка некоторых зафиксированных запросов и отправление ответов на них
+                if text.lower() == 'помощь':
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          'Список доступных команд: Принцип работы, Возможности, Ограничения, Средняя длина сообщений,'
+                                          'Средняя длина ответов.'
+                                          'Для вызова команды напишите соответствующее сообщение в чате.'})
+                    previous_message = 'Список доступных команд: Принцип работы, Возможности, Ограничения, Средняя длина сообщений,' \
+                                       'Средняя длина ответов.' \
+                                       'Для вызова команды напишите соответствующее сообщение в чате.'
+                elif text.lower() == 'принцип работы':
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          'Данный бот работает при помощи взаимодействия с нейросетью ChatGPT. При вводе в чат команды '
+                                          'отправляется соответствующий запрос, ответом на который является выводимый текст. '
+                                          'Точность ответа на вопрос может настраивать владелец бота, однако, при более точных ответах '
+                                          'время получения ответа на запрос значительно увеличивается'})
+                    previous_message = 'Данный бот работает при помощи взаимодействия с нейросетью ChatGPT. При вводе в чат команды ' \
+                                       'отправляется соответствующий запрос, ответом на который является выводимый текст. ' \
+                                       'Точность ответа на вопрос может настраивать владелец бота, однако, при более точных ответах ' \
+                                       'время получения ответа на запрос значительно увеличивается'
+                elif text.lower() == 'возможности':
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          'Я способен ответить на большинство запросов, которые могут быть заданы. '
+                                          'В качестве ответа может выступать не только текст сообщения, но и, например, код для программы,'
+                                          'если задан такой запрос.'})
+                    previous_message = 'Я способен ответить на большинство запросов, которые могут быть заданы. ' \
+                                       'В качестве ответа может выступать не только текст сообщения, но и, например, код для программы,' \
+                                       'если задан такой запрос.'
+                elif text.lower() == 'ограничения':
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          'Я способен ответить на большинство вопросов, но не на все. '
+                                          'Если данные, необходимые для ответа на запрос не находятся в открытом доступе, '
+                                          'у меня может не получиться найти ответ. Также я не могу дать ответ на события из реального мира, '
+                                          'которые произошли после 2020 года, поскольку я обучался до 2020 года.'})
+                    previous_message = 'Я способен ответить на большинство вопросов, но не на все. ' \
+                                       'Если данные, необходимые для ответа на запрос не находятся в открытом доступе, ' \
+                                       'у меня может не получиться найти ответ. Также я не могу дать ответ на события из реального мира, ' \
+                                       'которые произошли после 2020 года, поскольку я обучался до 2020 года.'
+                elif text.lower() == 'средняя длина сообщений':
+                    get_messages(user_id)
+                    average_message_length(user_id)
+                elif text.lower() == 'средняя длина ответов':
+                    get_answers(user_id)
+                    average_answer_length(user_id)
+                else:
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          gpt_callback(text)})
+                    previous_message = gpt_callback(text)
+
+                # Если сообщение на английском - задается вопрос о переводе
+                if translate:
+                    vk_session.method('messages.send',
+                                      {'user_id': user_id, 'random_id': random.randrange(0, 16), 'message':
+                                          'Похоже, ваше сообщение, как и ответ на него, написаны на английском языке, желаете перевести?'})
